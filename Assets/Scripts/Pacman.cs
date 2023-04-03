8823d68d4f8c341474932ab95b29f4fab5f3d98b
Francisco Igor Parente de Freitas <figorpdf@gmail.com>
2023-04-03 09:33:09 -0300

    enviar o codigo que falta

--- Assets/Scripts/Pacman.cs
+++ Assets/Scripts/Pacman.cs
@@ -12,22 +12,24 @@ public class Pacman : MonoBehaviour
     }
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetAxis("Vertical")>0 || Input.GetAxisRaw("Vertical") > 0)
         {
             this.moviment.SetDirection(Vector2.up);
         }
-        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetAxis("Vertical") < 0 || Input.GetAxisRaw("Vertical") < 0)
         {
             this.moviment.SetDirection(Vector2.down);
         }
-        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetAxis("Horizontal") < 0 || Input.GetAxisRaw("Horizontal") < 0)
         {
             this.moviment.SetDirection(Vector2.left);
         }
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetAxis("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") > 0)
         {
             this.moviment.SetDirection(Vector2.right);
         }
+        
+
 
         float angle = Mathf.Atan2(this.moviment.direction.y, this.moviment.direction.x);
         this.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
